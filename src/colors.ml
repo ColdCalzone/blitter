4b40592a8f51b863f75f48dc0892ef3c1b415856
pizzart <7o7goo7o7@gmail.com>
2021-07-12 14:23:22 +0300

    use terminal colors instead of hex values

--- src/colors.ml
+++ src/colors.ml
@@ -1,33 +1,21 @@
 open Core
 open Notty
 
-let color_of_hexstring str =
-  match Color.of_hexstring str with
-  | None -> failwith "Failed to derive color from hexstring"
-  | Some color ->
-    let rgba = Color.to_rgba color in
-    A.rgb_888 ~r:rgba.r ~g:rgba.g ~b:rgba.b
-;;
+let default_bg = A.(bg black)
+let selection_bg = A.(bg lightblack)
+
+let text = A.(fg lightwhite ++ default_bg)
+let ahead_gain = A.(A.(fg green ++ default_bg) ++ st bold)
+let ahead_loss = A.(fg yellow ++ default_bg)
+let behind_gain = A.(fg magenta ++ default_bg)
+let behind_loss = A.(A.(fg red ++ default_bg)++ st bold)
+let idle = A.(fg cyan ++ default_bg)
+let label = A.(fg white ++ default_bg)
 
 let color_of_hsl h s l =
   let hsl = Color.of_hsl h s l in
   let rgba = Color.to_rgba hsl in
   A.rgb_888 ~r:rgba.r ~g:rgba.g ~b:rgba.b
-;;
-
-let default_bg = A.(bg (color_of_hexstring "#2f3542"))
-let selection_bg = A.(bg (color_of_hexstring "#485460"))
-let attr_of_hexstring str = A.(fg (color_of_hexstring str) ++ default_bg)
-
-(* https://flatuicolors.com/palette/cn *)
-(* TODO Find better color palette *)
-let text = attr_of_hexstring "#f1f2f6"
-let ahead_gain = A.(attr_of_hexstring "#2ed573" ++ st bold)
-let ahead_loss = attr_of_hexstring "#7bed9f"
-let behind_gain = attr_of_hexstring "#ff6b81"
-let behind_loss = A.(attr_of_hexstring "#ff4757" ++ st bold)
-let idle = attr_of_hexstring "#1e90ff"
-let label = attr_of_hexstring "#a4b0be"
 
 let rainbow () =
   let period = 3. in
